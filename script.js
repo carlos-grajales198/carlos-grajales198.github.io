9361856dfe934a2f4e283a9d4817deae03f23fa6
Carlos Grajales <135731124+carlos-grajales198@users.noreply.github.com>
2023-06-07 00:17:54 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function show() {
+    const menu = document.getElementById('menu');
+    this.menu.style.left = '0';
+}
+
+function hide() {
+    const menu = document.getElementById('menu');
+    this.menu.style.left = '-100%';
+}
